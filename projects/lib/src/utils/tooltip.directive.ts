89ae4dddc3eadd4573aad27a0d227277095b05e9
TOME Olivier <olivier.tome@sinequa.com>
2022-03-21 08:59:33 +0100

    (tooltip) add tooltip on uib-configurable mouse over and hide on mouse leave

--- projects/lib/src/utils/tooltip.directive.ts
+++ projects/lib/src/utils/tooltip.directive.ts
@@ -1,32 +1,53 @@
-import { Directive, ElementRef, Input, OnChanges, OnDestroy } from "@angular/core";
+import { AfterContentInit, Directive, ElementRef, HostBinding, HostListener, Input, OnChanges, OnDestroy } from "@angular/core";
 import { Tooltip } from "bootstrap";
 
 @Directive({
   selector: '[uib-tooltip]'
 })
-export class TooltipDirective implements OnChanges, OnDestroy {
+export class TooltipDirective implements OnChanges, OnDestroy, AfterContentInit {
   @Input('uib-tooltip') html: string;
   @Input() placement: 'auto' | 'top' | 'bottom' | 'left' | 'right' = 'auto';
   @Input() container?: string | Element;
 
   tooltip?: Tooltip;
+  
+  @HostListener('mouseleave', ['$event']) mouseleave(event: MouseEvent) {
+    this.tooltip?.hide();
+  }
+  
+  // @Input() it's used to trigger the changes detection cycle for the directive
+  @HostBinding('disabled')
+  @Input() disabled: boolean;
 
   constructor(public el: ElementRef){}
 
   ngOnChanges() {
-    this.tooltip?.dispose(); // Force the creation of a new tooltip, or else the content is not updated
-    if(this.html) {
+    // when element is disabled, remove tooltip otherwise it will be shown/created when enabled
+    if (this.disabled && this.tooltip) {
+      this.tooltip?.dispose();
+    } else {
+      this.setTooltip();
+    }
+  }
+  
+  ngAfterContentInit(): void {
+    this.setTooltip();
+  }
+
+  ngOnDestroy() {
+    this.tooltip?.dispose();
+  }
+  
+  setTooltip() {
+    if (this.html) {
       this.tooltip = Tooltip.getOrCreateInstance(this.el.nativeElement, {
         html: true,
         placement: this.placement,
         sanitize: false,
         title: this.html,
+        delay: 300,
         container: this.container || this.el.nativeElement
       });
     }
-  }
-
-  ngOnDestroy() {
-    this.tooltip?.dispose();
   }
 }
