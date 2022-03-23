57f99cf1115ad2d32c9cb1c17cd54227de0f2a27
TOME Olivier <olivier.tome@sinequa.com>
2022-03-23 08:39:43 +0100

    (feat) add ngModelChangeDebounced directive

--- projects/lib/src/utils/public-api.ts
+++ projects/lib/src/utils/public-api.ts
@@ -2,3 +2,4 @@ export * from './template-name.directive';
 export * from './modal.component';
 export * from './tooltip.directive';
 export * from './utils.module';
+export * from './model-change.directive';
