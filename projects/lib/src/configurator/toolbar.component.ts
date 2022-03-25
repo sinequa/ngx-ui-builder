bfcd8e18b521ee024ef301d729d404e808c994e8
TOME Olivier <olivier.tome@sinequa.com>
2022-03-25 16:13:12 +0100

    (toolbar) custom svg to open/close button

--- projects/lib/src/configurator/toolbar.component.ts
+++ projects/lib/src/configurator/toolbar.component.ts
@@ -5,27 +5,30 @@ import { ConfigService } from "../configuration";
 @Component({
   selector: 'uib-toolbar',
   template: `
-<div class="btn-group uib-toolbar-anim" *ngIf="{enabled: configurableService.editorEnabled$ | async} as editor" >
-  <button class="btn bg-warning" (click)="toggleEditor()">
-    <span *ngIf="editor.enabled" class="fa-stack">
-      <i class="fas fa-pen fa-stack-1x"></i>
-      <i class="fas fa-ban fa-stack-2x"></i>
-    </span>
-    <i *ngIf="!editor.enabled" class="fas fa-pen"></i>
-  </button>
+<div class="btn-group uib-toolbar uib-toolbar-anim" *ngIf="{enabled: configurableService.editorEnabled$ | async} as editor" >
   <ng-container *ngIf="editor.enabled">
     <button class="btn btn-primary" (click)="configService.undo()" [disabled]="!(configService.canUndo$()| async)" uib-tooltip="Undo" placement="top">
-      <svg-icon key="undo" width="22px" height="25px"></svg-icon>
+      <svg-icon key="undo"></svg-icon>
     </button>
     <button class="btn btn-primary" (click)="configService.redo()" [disabled]="!(configService.canRedo$()| async)" uib-tooltip="Redo" placement="top">
-      <svg-icon key="redo" width="22px" height="25px"></svg-icon>
+      <svg-icon key="redo"></svg-icon>
     </button>
   <ng-content></ng-content>
   </ng-container>
+  <button class="btn bg-warning" (click)="toggleEditor()">
+    <svg-icon *ngIf="editor.enabled" key="eye_slash"></svg-icon>
+    <svg-icon *ngIf="!editor.enabled" key="eye"></svg-icon>
+  </button>
 </div>
   `,
   styles: [
     `
+    .uib-toolbar .btn svg-icon {
+      display: flex;
+      align-items: center;
+      width: 22px;
+      height: 25px;
+    }
     .uib-toolbar-anim {
       transition: ease .3s;
     }
