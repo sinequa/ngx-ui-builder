c2ffaa66ad7bda9363a54e3d110d404a56f2af35
Leibenguth <leibenguth@sinequa.com>
2022-03-28 17:24:23 +0200

    Improve toolbar API

--- projects/lib/src/configurator/toolbar.component.ts
+++ projects/lib/src/configurator/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ContentChild, TemplateRef } from "@angular/core";
 import { ConfigurableService } from "../configurable";
 import { ConfigService } from "../configuration";
 
@@ -7,15 +7,15 @@ import { ConfigService } from "../configuration";
   template: `
 <div class="btn-group uib-toolbar uib-toolbar-anim" *ngIf="{enabled: configurableService.editorEnabled$ | async} as editor" >
   <ng-container *ngIf="editor.enabled">
+    <ng-container [ngTemplateOutlet]="template"></ng-container>
     <button class="btn btn-primary" (click)="configService.undo()" [disabled]="!(configService.canUndo$()| async)" uib-tooltip="Undo" placement="top">
       <svg-icon key="undo"></svg-icon>
     </button>
     <button class="btn btn-primary" (click)="configService.redo()" [disabled]="!(configService.canRedo$()| async)" uib-tooltip="Redo" placement="top">
       <svg-icon key="redo"></svg-icon>
     </button>
-    <ng-content></ng-content>
   </ng-container>
-  <button class="btn bg-warning" (click)="toggleEditor()">
+  <button class="btn btn-primary" (click)="toggleEditor()">
     <svg-icon *ngIf="editor.enabled" key="eye_slash"></svg-icon>
     <svg-icon *ngIf="!editor.enabled" key="eye"></svg-icon>
   </button>
@@ -37,7 +37,7 @@ import { ConfigService } from "../configuration";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ToolbarComponent {
-  
+  @ContentChild(TemplateRef, {static: false}) template: TemplateRef<any>;
   
   constructor(
     public configService: ConfigService,
