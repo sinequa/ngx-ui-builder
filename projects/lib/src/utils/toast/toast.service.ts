042f8049f26a4abd8cd8312aec8fc96968016dda
Leibenguth <leibenguth@sinequa.com>
2022-10-24 16:13:12 +0200

    Improved toast API and displayed a toast with Undo when a component is removed

--- projects/lib/src/utils/toast/toast.service.ts
+++ projects/lib/src/utils/toast/toast.service.ts
@@ -1,13 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Toast } from 'bootstrap';
 import { Subject } from 'rxjs';
+
+export type BsStyle = 'info' | 'success' | 'warning' | 'danger' | 'primary' | 'secondary' | 'light' | 'dark';
+
+export interface ToastAction {
+  text?: string;
+  style?: string | string;
+  icon?: string;
+  hideToast?: boolean;
+  action: () => void;
+}
+
+export interface ToastMessage {
+  icon?: string;
+  message: string;
+  style?: BsStyle | string;
+  actions?: ToastAction[];
+  options?: Toast.Options;
+}
 
 @Injectable({providedIn: 'root'})
 export class ToastService {
-  
-  onToastMessage = new Subject<string>();
-  
+
+  onToastMessage = new Subject<ToastMessage|null>();
+
   constructor() {}
-  
-  info(message:string) { this.onToastMessage.next(message) }
-  
+
+  show(
+    message: string,
+    style: BsStyle | string = 'info',
+    actions?: ToastAction[],
+    options?: Toast.Options
+  ) {
+    this.onToastMessage.next({message, style, actions, options});
+  }
+
+  hide() {
+    this.onToastMessage.next(null);
+  }
+
 }
