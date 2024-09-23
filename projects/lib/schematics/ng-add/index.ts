b1c4a49e9b4431a0d5153476b7ec366d2c6cbdf3
Mansour Fall <mansour.fall@gebeya.com>
2024-09-23 01:52:46 +0000

    update package name and schematics

--- projects/lib/schematics/ng-add/index.ts
+++ projects/lib/schematics/ng-add/index.ts
@@ -4,15 +4,15 @@ import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angula
 
 const packageJson = {
   peerDependencies: {
-    "@ngrx/store": "^14.3.2",
+    "@ngrx/store": "^15.0.0",
     "@popperjs/core": "^2.11.5",
     "bootstrap": "^5.2.0",
     "immer": "^9.0.16",
-    "ngrx-wieder": "^9.0.0",
+    "ngrx-wieder": "^10.0.2",
     "ngx-drag-drop": "^2.0.0"
   },
   devDependencies: {
-    "@angular/localize": "^14.1.0",
+    "@angular/localize": "^15.0.0",
     "@types/bootstrap": "^5.2.0",
     "htmlparser": "1.7.7",
     "html-prettify": "1.0.7",
@@ -20,9 +20,37 @@ const packageJson = {
   }
 }
 
+function addImportsToStyles(host: Tree) {
+  const stylesPath = 'src/styles.scss';
+  const importsToAdd = `
+/* bootstrap's utilities only */
+@import "bootstrap/scss/mixins/banner";
+@include bsBanner(Utilities);
+@import "bootstrap/scss/functions";
+@import "bootstrap/scss/variables";
+@import "bootstrap/scss/maps";
+@import "bootstrap/scss/mixins";
+@import "bootstrap/scss/utilities";
+@import "bootstrap/scss/root";
+@import "bootstrap/scss/helpers";
+@import "bootstrap/scss/utilities/api";
+
+@import "../node_modules/@gsaas/ngx-ui-builder/styles/ui-builder.scss";
+`;
+
+  if (host.exists(stylesPath)) {
+    const originalContent = host.read(stylesPath)!.toString('utf-8');
+    const updatedContent = importsToAdd + '\n' + originalContent;
+    host.overwrite(stylesPath, updatedContent);
+  } else {
+    host.create(stylesPath, importsToAdd);
+  }
+}
+
 // Just return the tree
 export function ngAdd(): Rule {
   return (tree: Tree, context: SchematicContext) => {
+    addImportsToStyles(tree);
 
     // Add peerDependencies to the dependencies of the app
     Object.keys(packageJson.peerDependencies)
