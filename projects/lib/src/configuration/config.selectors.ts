8cadde07a0679164ba810f08ea3fa2d10de8af4f
Hebus <olivier.tome@sinequa.com>
2025-02-04 17:47:58 +0100

    Angular 18 (#110)
    * Remove deprecated documentation files and test configurations
    
    * Remove outdated browserslist files and update Angular dependencies to version 15
    
    * Remove demo runtime and index HTML files
    
    * Update Angular output path, bump ngrx and ngrx-wieder dependencies, and remove jasmine type
    
    * Upgrade @ngrx/store to version 15.0.0 and update related types in selectors and reducer
    
    * Upgrade Angular and NgRx dependencies to version 16
    
    * Update Angular dependencies to version 17 and adjust build targets in angular.json
    
    * Upgrade @ngrx/store, ngrx-wieder, and ngx-drag-drop to version 17, and update related dependencies in package.json and package-lock.json
    
    * Upgrade Angular and NgRx dependencies to version 18, update tsconfig and angular.json configurations, and adjust HttpClient usage in main.ts
    
    * Update @schematics/angular to version 18.2.14 in package.json

--- projects/lib/src/configuration/config.selectors.ts
+++ projects/lib/src/configuration/config.selectors.ts
@@ -1,11 +1,11 @@
 import { createSelector } from "@ngrx/store";
 import { createHistorySelectors } from "ngrx-wieder";
-import { uibConfig } from "./config.reducer";
+import { uibConfig, ConfigState } from "./config.reducer";
 
 const selectConfig = uibConfig.selectUibConfigState;
 
-export const selectAll = createSelector(selectConfig, s => Object.values(s.config));
-export const selectItem = (id: string) => createSelector(selectConfig, s => s.config[id]);
+export const selectAll = createSelector(selectConfig, (s: ConfigState) => Object.values(s.config));
+export const selectItem = (id: string) => createSelector(selectConfig, (s: ConfigState) => s.config[id]);
 export const {
   selectCanUndo,
   selectCanRedo,
